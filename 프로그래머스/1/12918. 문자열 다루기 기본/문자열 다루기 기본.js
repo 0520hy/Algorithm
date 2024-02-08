fd26e3f24049b60475ec1495cb60e97a3f54608a
0520hy <103192902+0520hy@users.noreply.github.com>
2024-02-09 01:16:01 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,13 @@
+function solution(s) {
+
+    
+     if((s.length == 4 || s.length == 6)&& /^\d+$/.test(s)){
+        return true;
+         
+     }else{
+         return false;
+     }
+    
+    
+    
+}
