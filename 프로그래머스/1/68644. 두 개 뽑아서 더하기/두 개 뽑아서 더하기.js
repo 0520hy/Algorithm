cc184675e82228a8900d7b0d7058fc2326e716f5
0520hy <103192902+0520hy@users.noreply.github.com>
2024-02-28 17:41:44 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.29 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,16 @@
+function solution(numbers) {
+    var answer = [];
+    
+    for( i = 0; i < numbers.length; i++){
+        for( j = 0; j < numbers.length; j++){
+            if(i !== j){
+                answer.push(numbers[i] + numbers[j]);
+            }
+        }
+    }
+    
+    answer = [...new Set(answer)];
+    
+    
+    return answer.sort((a,b)=>a-b);
+}
