5d5487d7b917eb4adc2a3559ace2acc513d4fea8
0520hy <103192902+0520hy@users.noreply.github.com>
2024-03-06 12:12:37 +0900

    Update JadenCase 문자열 만들기.js

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -3,6 +3,8 @@ function solution(s) {
    
    let tmp = s.toLowerCase();
     
-   return tmp.replace(/\b[a-z]/g, letter => letter.toUpperCase())
+   return tmp.replace(/\b[a-z]/g, first => first.toUpperCase())
+    // \b 문자의 시작 부분 찾기
+    // g 전역 검색
     
 }
