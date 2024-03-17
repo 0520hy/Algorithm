b2092ba42d17318410683f749542bdc6410586bd
0520hy <103192902+0520hy@users.noreply.github.com>
2024-03-17 19:10:33 +0900

    [level 1] Title: 삼총사, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,17 @@
+function solution(number) {
+    let answer = 0;
+    let sum = 0;
+    
+    for(i=0; i<number.length; i++){
+        for(j=0; j<number.length; j++){
+            for(k=0; k<number.length; k++){
+                if( i !== j && j !== k && i !== k){
+                    if(number[i] + number[j] + number[k] == 0 ){
+                        answer ++;
+                    }
+                }
+            }
+        }
+    }
+    return answer/6;
+}
