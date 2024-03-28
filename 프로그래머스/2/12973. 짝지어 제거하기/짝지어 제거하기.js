7ed91992a3a938724d004fb2f7ab037213e26455
0520hy <103192902+0520hy@users.noreply.github.com>
2024-03-28 13:34:32 +0900

    [level 2] Title: 짝지어 제거하기, Time: 25.69 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,17 +1,19 @@
 function solution(s) {
-    let answer = 0;
 
     let arr = [];
-
-    for(let i = 0; i < s.length; i++) {
-
-
-        if(s[i] == arr[arr.length - 1]) {
-           arr.pop(); 
-        } else {
-            arr.push(s[i]);
+    
+    if(s.length  % 2 == 1 ) return 0
+    
+    for(i = 0; i < s.length; i++){
+        if( s[i] == arr[arr.length - 1]){
+            arr.pop();
+        }else{
+            arr.push(s[i])
         }
+        
+        
     }
 
-    return arr.length == 0 ? 1 : 0;
+   return arr.length == 0 ? 1 : 0;
+    
 }
