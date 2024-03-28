ba83173e2506e8465c20d623e266cbee8479e1e6
0520hy <103192902+0520hy@users.noreply.github.com>
2024-03-28 14:32:33 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -2,10 +2,12 @@ function solution(food) {
     var answer = '';
     
     for( i = 1; i < food.length; i++){
-        let num = food[i] / 2;
-        for( k = 1; k <= num; k++){
-            answer += i
-        }
+        let num = i.toString();
+        answer += num.repeat((food[i] / 2))
+       
+        // for( k = 1; k <= num; k++){
+        //     answer += i
+        // }
     }
     
      let reverse = answer.split("").reverse().join("");
