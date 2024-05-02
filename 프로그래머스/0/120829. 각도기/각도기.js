38814c7486c85a557afe9fe7e9bd22eeac8e193f
0520hy <103192902+0520hy@users.noreply.github.com>
2024-05-02 20:51:25 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,14 +1,3 @@
 function solution(angle) {
-    var answer = 0;
-    
-    if(0 < angle && angle < 90){
-        return 1;
-    }else if(angle == 90){
-        return 2;
-    }else if(90 < angle && angle < 180){
-        return 3;
-    }else if(angle == 180){
-        return 4;
-    }
-
+  return angle < 90 ? 1 : angle == 90 ? 2 : angle < 180 ? 3 : 4;
 }
