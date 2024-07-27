390f69a7085f9d768b50992b01a407f4e04eaebb
0520hy <103192902+0520hy@users.noreply.github.com>
2024-07-28 02:41:07 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,9 +1,3 @@
 def solution(my_string):
-    answer = ''
-    
-    for i in my_string:
-        if i.isupper():
-            answer += i.lower()
-        elif i.islower():
-            answer += i.upper()
-    return answer
+
+    return my_string.swapcase()
