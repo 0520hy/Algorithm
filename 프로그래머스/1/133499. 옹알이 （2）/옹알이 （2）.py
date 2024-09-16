241b252bb1bfd3f5c69dde3bde5fff690fd71d7f
0520hy <103192902+0520hy@users.noreply.github.com>
2024-09-16 23:08:34 +0900

    [level 1] Title: 옹알이 (2), Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,15 +1,11 @@
-import re
-
 def solution(babbling):
-    answer = 0
-    
-    pattern = re.compile(r'^(aya|ye|woo|ma)+$')
 
+    answer = 0
     for i in babbling:
         if "ayaaya" in i or "yeye" in i or "woowoo" in i or "mama" in i:
-            continue
-        
-        if pattern.match(i):
+                continue
+        else:
+            i = i.replace('aya',' ').replace('ye',' ').replace('woo',' ').replace('ma',' ').replace(' ', '')
+        if i == "":
             answer += 1
-    
     return answer
