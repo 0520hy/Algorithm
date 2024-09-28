c01e27f803df66dc2f85ec2d27967e31f793287d
0520hy <103192902+0520hy@users.noreply.github.com>
2024-09-28 17:09:43 +0900

    [level 1] Title: 기사단원의 무기, Time: 147.22 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,19 +1,12 @@
-import math
-
 def solution(number, limit, power):
-    div = []
+    div = [0] * (number + 1)
     answer = 0
     
     for i in range(1, number + 1):
-        cnt = 0
-        for j in range(1, int(math.sqrt(i)) + 1):
-            if i % j == 0:
-                cnt += 1
-                if j != i // j: 
-                    cnt += 1
-        div.append(cnt)
-        
-    for i in div:
+        for j in range(i, number + 1, i):
+            div[j] += 1
+    
+    for i in div[1:]:  
         if i <= limit:
             answer += i
         else:
